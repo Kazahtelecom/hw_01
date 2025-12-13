502da82c8bafee454ccbd0bf1234de8e5e7e4605
Kuandykov Zhasulan <kz4444gron.nkk@gmail.com>
2025-12-13 11:12:19 +0500

    copy start code

---
+++ lab_01.py
@@ -0,0 +1,17 @@
+import time
+import random
+
+# Генерация данных
+n = 100000
+data = [random.randint(1, n // 2) for _ in range(n)]
+target = random.choice(data)
+
+# TODO: Измерьте время поиска в списке
+# start = time.perf_counter()
+# ...
+
+# TODO: Измерьте время поиска в множестве
+# ...
+
+# TODO: Выведите результаты
+            
