536be1f0c8c1ea3735bbbd19bb23798f14967e85
Kuandykov Zhasulan <kz4444gron.nkk@gmail.com>
2025-12-13 11:20:45 +0500

    1)Generate random list of 100,000 numbers 2)Compare linear search in list vs hash-based search in set 3)Demonstrate O(n) vs O(1) time complexity difference 4)Show performance improvement of set over list"

--- lab_01.py
+++ lab_01.py
@@ -6,12 +6,24 @@ n = 100000
 data = [random.randint(1, n // 2) for _ in range(n)]
 target = random.choice(data)
 
-# TODO: Измерьте время поиска в списке
-# start = time.perf_counter()
-# ...
+start = time.time()
+target in data 
+end = time.time()
+list_time = end - start
 
-# TODO: Измерьте время поиска в множестве
-# ...
+data_set = set(data)
 
-# TODO: Выведите результаты
-            
+start = time.time()
+target in data_set 
+end = time.time()
+set_time = end - start
+
+print(f"Time taken to search in list: {list_time:.6f} seconds")
+print(f"Time taken to search in set: {set_time:.6f} seconds")
+print(f"Set search is {list_time / set_time:.2f} times faster than list search")
+print(f"Data size: {n} elements")
+print(f"Target value: {target}")
+
+
+
+
