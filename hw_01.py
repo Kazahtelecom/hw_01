1a767fb67818a480362a8f0495ef3103f6712adf
Kazahtelecom <kz4444gron.nkk@gmail.com>
2025-12-13 11:44:24 +0500

    копирование использование множество

--- hw_01.py
+++ hw_01.py
@@ -8,3 +8,12 @@ def find_common_naive(arr1, arr2):
         if item in arr2 and item not in common:
             common.append(item)
     return common
+
+def find_common_set(arr1, arr2):
+    """
+    Подход с множествами: O(n + m)
+    Используем встроенную операцию пересечения множеств
+    """
+    set1 = set(arr1)
+    set2 = set(arr2)
+    return list(set1 & set2)
