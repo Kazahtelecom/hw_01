d1a99dd71d58310f6f15dcf4511696e35d6de8d0
Kuandykov Zhasulan <kz4444gron.nkk@gmail.com>
2025-12-13 11:22:32 +0500

    comment

--- lab_01.py
+++ lab_01.py
@@ -6,18 +6,19 @@ n = 100000
 data = [random.randint(1, n // 2) for _ in range(n)]
 target = random.choice(data)
 
+# Поиск в списке
 start = time.time()
 target in data 
 end = time.time()
 list_time = end - start
-
+# Поиск в множестве
 data_set = set(data)
-
+# Измерение времени поиска в множестве
 start = time.time()
 target in data_set 
 end = time.time()
 set_time = end - start
-
+# Вывод результатов
 print(f"Time taken to search in list: {list_time:.6f} seconds")
 print(f"Time taken to search in set: {set_time:.6f} seconds")
 print(f"Set search is {list_time / set_time:.2f} times faster than list search")
