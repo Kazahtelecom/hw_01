9a59c6cf8dc1fcc93a29adfc9df9e4d1596f6c2d
Kazahtelecom <kz4444gron.nkk@gmail.com>
2026-01-05 20:55:15 +0500

    start v2 home work

---
+++ Lab-Homework v2/Homew_01/hw_01v2.py
@@ -0,0 +1,7 @@
+def reverse_string(s: str) -> str:
+    """
+    reverse_string("hello") → "olleh"
+    reverse_string("Алматы") → "ытамлА"
+    """
+    # Используйте стек: push все символы, затем pop
+    pass
