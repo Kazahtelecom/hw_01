3f7b6df987c7f5997ec562d50999d9416206923e
Kazahtelecom <kz4444gron.nkk@gmail.com>
2026-01-05 20:59:17 +0500

    Первое задание

--- Lab-Homework v2/Homew_01/hw_01v2.py
+++ Lab-Homework v2/Homew_01/hw_01v2.py
@@ -1,7 +1,16 @@
 def reverse_string(s: str) -> str:
+    stack = []
+    for char in s:
+        stack.append(char)
+
+    reversed_s = ''
+    while stack:
+        reversed_s += stack.pop()
+    return reversed_s 
     """
     reverse_string("hello") → "olleh"
     reverse_string("Алматы") → "ытамлА"
     """
     # Используйте стек: push все символы, затем pop
+
     pass
